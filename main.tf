314cc2829bfa7e6ab1da097ab7cb084ddd7fc349
Noah Mercado <noah.mercado@Noah.Mercadl>
2020-02-06 16:45:09 -0500

    update moduleName and version

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "null_resource" "example" {
 
   provisioner "local-exec" {
 
-    command = format("echo moduleName: %s", local.moduleVersion)
+    command = format("echo moduleName: %s", local.moduleName)
   }
+
+  provisioner "local-exec" {
+
+    command = format("echo moduleVersion: %s", local.moduleVersion)
+  }
+
 }
